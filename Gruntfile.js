871cb6ef0577daf6576a6a370f03d09211c76159
aditmehta <aditmehta10@gmail.com>
2015-08-27 15:26:49 +0530

    finished home page

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,6 @@ module.exports = function(grunt){
         'assets/stylesheets/home.css': 'assets/stylesheets/home.scss',       
         'assets/stylesheets/services.css': 'assets/stylesheets/services.scss',
         'assets/stylesheets/about.css': 'assets/stylesheets/about.scss',
-        'assets/stylesheets/portfolio.css': 'assets/stylesheets/portfolio.scss',
-        'assets/stylesheets/testimonials.css': 'assets/stylesheets/testimonials.scss',
-        'assets/stylesheets/contact.css': 'assets/stylesheets/contact.scss'
       }
     }
   },
@@ -23,8 +20,7 @@ module.exports = function(grunt){
     target: {
       files: {
         'output.css': ['assets/stylesheets/styles.css','assets/stylesheets/home.css',
-        'assets/stylesheets/services.css','assets/stylesheets/about.css','assets/stylesheets/portfolio.css',
-        'assets/stylesheets/testimonials.css','assets/stylesheets/contact.css']
+        'assets/stylesheets/services.css','assets/stylesheets/about.css']
       }
     }
   }
